e3ca69c4c8394a5322f7100fb64cd6dccdbaaa63
ahmed manai <elmanai5400@gmail.com>
2023-08-22 16:48:13 +0100

    mon premier commit

---
+++ src/main/java/com/whizpath/nutritionreport/model/MacroNutrient.java
@@ -0,0 +1,12 @@
+package com.whizpath.nutritionreport.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MacroNutrient {
+    private  String macroNutrientName;
+    private  int macroNutrientValue;
+
+}
